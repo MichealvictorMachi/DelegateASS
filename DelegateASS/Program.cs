0ae9cb967f5ee276c2ee4d7a96570077151a105d
Victor Micheal <michealvictor175@gmail.com>
2022-08-15 22:20:52 +0100

    Add project files.

---
+++ DelegateASS/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegateASS
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Shows the timer.
+        /// </summary>
+        /// <param name="seenTime">The seen time.</param>
+        public static void ShowTimer(int seenTime)
+        {
+            Console.WriteLine($"watched scenes is : {seenTime} ");
+        }
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            VideoPlayer.MovieWatch(ShowTimer);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DelegateASS/VideoPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateASS
+{
+    /// <summary>
+    /// delegate method that will be passed as parameters into the main method
+    /// </summary>
+    internal class VideoPlayer
+    {
+        public delegate void Duration(int scenesSeen);
+        public static void MovieWatch(Duration durt)
+        {
+            for (int i = 0; i < 7236; i++)
+            {
+                var scenesSeen = i.ToString();
+                durt(i);
+            }
+        }
+    }
+}
